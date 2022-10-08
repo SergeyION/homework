42c6b675a005b5c6272872f98b5cc7fecb3478c5
Sergey Ionov <captor_72@bk.ru>
2022-10-08 20:32:33 +0700

    add program.cs

--- homework/task002/Program.cs
+++ homework/task002/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a = 5;
+int b = 7;
+
+int max = a;
+int min = b;
+
+if(b > max) max = b;
+{
+
+}
+
+else(a = min);
+
+Console.Write("max = ");
+Console.WriteLine(max);
+Console.Write("min = ");
+Console.WriteLine(min);
+
