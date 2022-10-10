09db63c8bc8ff8c8dabf791d34cedfe83e28ae2b
Sergey Ionov <captor_72@bk.ru>
2022-10-10 19:39:50 +0700

    Решение задачи 6 с диаграммой

--- homework/task006/Program.cs
+++ homework/task006/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a = 7; 
+if (a % 2 == 0) // (|a % 5 == 0||a % 3 == 0||a % 6 == 0||a % 9 == 0) 
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
