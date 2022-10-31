b3c3c7e6ba2f6ee17dc9140577b78b6b331c38e5
Sergey Ionov <captor_72@bk.ru>
2022-10-31 22:12:11 +0700

    решаем задачи

---
+++ examples/task007/Program.cs
@@ -0,0 +1,5 @@
+﻿// // Ограничение вызова знаков после запятой.
+// double d = 123.123456789;
+// Console.WriteLine(string.Format("{0:F2}", d)); // 123,12
+// Console.WriteLine($"{d:F4}", d); // 123,1235
+// Console.WriteLine(d.ToString("### ###.### ###")); //123,123 457
--- homework/task013/Program.cs
+++ homework/task013/Program.cs
@@ -7,7 +7,7 @@ while (true)
 {
     string input = Console.ReadLine(); // локальная переменная / считывает введёное значение
     if(!input.Equals("exit")) 
-        Console.WriteLine("{0}->{1}", input, input[1]); // прогоняет две цифры из трёх с индексом [0] и [1]
+        Console.WriteLine("{0}->{2}", input, input[1]); // прогоняет две цифры из трёх с индексом [0] и [1]
         else
             break;
         // Console.WriteLine("{1}->{2}", input[1], input[2]);
